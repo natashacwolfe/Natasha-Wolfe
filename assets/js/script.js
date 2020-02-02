63fbc2e3ff55caf5c59962ae82b4b2d81bc10ac9
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-02-02 09:04:48 -0800

    added nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,15 @@
 let compass = document.querySelector('.compass');
-let links = []
+let navLinks = document.querySelector('.nav-links')
 
 
-function loadNavLinks(event) {
+
+function toggleNav(event) {
     console.log(event)
-    links = [
-        {
-            name:'About',
-            link: 'about.html'
-        },
-        {
-            name: 'Portfolio',
-            link: 'portfolio.html'
-        },
-        {
-            name: 'Contact',
-            link: 'contact.html'
-        }
-    ]
-    createLinks(links)
+   if (navLinks.style.display === 'none')
+   navLinks.style.display = 'flex';
+   else navLinks.style.display = 'none';
 }
 
-function createLinks(links){
-    console.log(links)
-    let navLinks = document.querySelector('nav-links');
 
-    for (let i = 0; i < links[i].length; i++) {
-        let linkName = links.name;
-        let linkHref = links.link;
-        let linkItem = document.createElement('li');
-        let linkTag = document.createElement('a');
 
-        console.log(linkName, linkHref)
-        
-        linkItem.setAttribute('class', 'liItem');
-        // linkTag.setAttribute({
-        //     'class': 'liLink',
-        //     // 'src' : linkHref
-        // });
-
-        linkItem.textContent = linkName;
-
-        linkItem.appendChild(linkTag);
-        navLinks.appendChild(linkItem)
-    }
-
-}
-
-compass.addEventListener('click', loadNavLinks)
+compass.addEventListener('mouseover', toggleNav)
