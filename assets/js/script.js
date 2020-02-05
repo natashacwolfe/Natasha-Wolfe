ef96f983607566eb211309dc91a3890359a7d907
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-02-04 17:08:49 -0800

    updated images and contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
-let compass = document.querySelector('.compass');
-let navLinks = document.querySelector('.nav-links');
+
 let logoImage = document.getElementById('main-image');
 let switchImages = ["./assets/images/1.png", "./assets/images/2.png", "./assets/images/3.png", "./assets/images/4.png"]
 let i = 0;
-
+let smallLogoImage = document.getElementById('small-main-image')
+let smallSwitchImages = ['./assets/images/1small.png', './assets/images/2small.png', './assets/images/3small.png','./assets/images/4small.png' ]
+let j = 0;
 // function toggleImg(event){
 //     console.log(event);
 //     console.log(switchImages)
@@ -13,16 +14,11 @@ let i = 0;
 // }
 let timer = setInterval(toggleImg, 800);
 
-function toggleNav(event) {
-    console.log(event)
-   if (navLinks.style.display === 'none')
-   navLinks.style.display = 'flex';
-   else navLinks.style.display = 'none';
-};
 
 function toggleImg(){
     console.log("done")
     logoImage.style.background = `url('${switchImages[i]}')`;
+    logoImage.style.backgroundRepeat="no-repeat";
     console.log(logoImage);
     if (i < switchImages.length - 1){
         i++;
@@ -30,6 +26,19 @@ function toggleImg(){
         i = 0;
     }
 }
+let smallImgTimer = setInterval(toggleImgSmall, 800);
 
-compass.addEventListener('mouseover', toggleNav);
+
+function toggleImgSmall(){
+    console.log("done")
+    smallLogoImage.style.background = `url('${smallSwitchImages[i]}')`;
+    smallLogoImage.style.backgroundRepeat="no-repeat";
+    console.log(logoImage);
+    if (i < smallSwitchImages.length - 1){
+        j++;
+    } else {
+        j = 0;
+    }
+}
+
 // logoImage.addEventListener('mouseover', toggleImg);
